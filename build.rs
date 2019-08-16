4df0f4ef49f958ba1ec217aebb499e0b708fac12
Лач <iam@lach.pw>
2019-08-17 03:11:33 +0500

    Embed VMProtectSDK .dll/.so into crate

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    println!("cargo:rustc-link-search=native=./lib/");
+    match env::var("TARGET").unwrap().as_ref() {
+        "x86_64-pc-windows-msvc" | "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK64"),
+        "x86-pc-windows-msvc" | "x86-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK32"),
+        v => panic!("Unsupported target: {}", v),
+    }
+}
