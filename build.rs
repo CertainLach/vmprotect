490880240005b0375209ba4e9658831442a124a9
Лач <iam@lach.pw>
2019-08-19 16:10:25 +0500

    Fix build script (use absolute path to project dir)

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-link-search=native=./lib/");
+    println!("cargo:rustc-link-search=native={}/lib/", env::var("CARGO_MANIFEST_DIR").unwrap());
     match env::var("TARGET").unwrap().as_ref() {
         "x86_64-pc-windows-msvc" | "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK64"),
         "x86-pc-windows-msvc" | "x86-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK32"),
