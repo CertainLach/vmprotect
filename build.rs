e66575bf0447acca16048a3ed63cc5f46d982087
Лач <iam@lach.pw>
2019-09-09 07:28:54 +0500

    Fix msvc target name

--- build.rs
+++ build.rs
@@ -10,10 +10,9 @@ fn main() {
         "x86_64-pc-windows-msvc" |
         "x86_64-pc-windows-gnu" |
         "x86_64-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK64"),
-        "x86-pc-windows-msvc" |
+        "i686-pc-windows-msvc" |
         "i686-unknown-linux-gnu" |
-        "i686-pc-windows-gnu" |
-        "x86-unknown-linux-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK32"),
+        "i686-pc-windows-gnu" => println!("cargo:rustc-link-lib=dylib=VMProtectSDK32"),
         v => panic!("Unsupported target: {}", v),
     }
 }
